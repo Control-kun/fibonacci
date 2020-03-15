5d3d796e433642a132f61e7e810b01c961896131
Khvostov Pavel <hvostovp@gmail.com>
2020-03-16 01:08:03 +0300

    feat: refactor fibo controller

--- app/Http/Controllers/Api/FiboController.php
+++ app/Http/Controllers/Api/FiboController.php
@@ -4,11 +4,54 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FiboController extends Controller
 {
-    public function getFibonacciSequence()
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getFibonacciSequence(Request $request)
     {
-        dd(1);
+        $validator = Validator::make($request->all(), [
+            'from' => 'required|integer',
+            'to' => 'required|integer|min:' . ((int)$request->from + 1),
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->toJson()], 422);
+        }
+
+        $result = [];
+        foreach ($this->fibonacci($request->from, $request->to) as $fib) {
+            $result[] = $fib;
+        }
+
+        return response()
+            ->json(['data' => $result], 200);
+    }
+
+    /**
+     * @param $from
+     * @param $to
+     * @return \Generator
+     */
+    function fibonacci($from, $to)
+    {
+        $a = 0;
+        $b = 1;
+        while ($to > 0) {
+            if ($from > 0) {
+                $from--;
+            } else {
+                yield $a;
+            }
+
+            $tmp = $a + $b;
+            $a = $b;
+            $b = $tmp;
+            $to--;
+        }
     }
 }
